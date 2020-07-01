bc7c612763fbbc4cea54432ba97f8f5f8c4a9c55
tamago1509 <54825087+tamago1509@users.noreply.github.com>
2020-07-01 22:57:18 +0700

    upload img in cloudinary

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,8 +1,21 @@
-
+require("dotenv").config();
+var express = require('express');
+var multer  = require('multer')
+var validate = require('../validate/user.validate');
 var db = require("../db");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 var shortId = require('shortId');
+var cloudinary = require('cloudinary').v2;
+
+var upload = multer({ dest: './public/uploads/' })
+
+
+cloudinary.config({ 
+  cloud_name: 'ngocduongflyinthesky1509', 
+  api_key: process.env.api_key, 
+  api_secret: process.env.api_secret 
+});
 
 module.exports.index= function(req, res){
 	res.render('users/index',{
@@ -17,25 +30,19 @@ module.exports.delete= function(req, res){
 	.write();
 	res.redirect('/users/index');
 }
-module.exports.update = function(req, res){
-	res.render('users/update',{
-		id: req.params.id
-	})
-}
-module.exports.postUpdate = function(req, res){
-	var updateId = req.params.id;
-	db.get('users')
-	.find({ id: updateId})
-	.assign({ name: req.body.name, email: req.body.email})
-	.write();
-	res.redirect('/users/index')
-}
 
 // handle sign up
 module.exports.postIndex = function(req, res){
 	//check xem email ton tai chua? nếu tồn tại thì bắt nhập lại mail khác
 	//nếu không thì lưu vào database
 	let email = req.body.email;
+	
+	req.body.avatar = req.file.path.split('\\').slice(1).join('/');
+	var file = req.body.avatar;
+	
+	cloudinary.uploader.upload("./public/" + file , function(error, result){
+		console.log(result, error); 
+	});
 
 	var userEmail = db.get('users').find({ email: email}).value();
 	if(userEmail){
@@ -53,11 +60,50 @@ module.exports.postIndex = function(req, res){
     			id: shortId.generate(),
     			name: req.body.name,
     			email: req.body.email,
-    			password: hash
+    			password: hash,
+    			avatarURL: req.body.avatar
     		}).write();
 			res.redirect('index')
 		});
-		
-		
 	}
+
+}	
+
+
+module.exports.update = function(req, res){
+	res.render('users/update',{
+		id: req.params.id,
+	})
 }
+
+module.exports.postUpdate = function(req, res){
+	var updateId = req.params.id;
+	req.body.avatar = req.file.path.split('\\').slice(1).join('/');
+	var file = req.body.avatar;
+	console.log(file);
+
+
+
+	cloudinary
+	.uploader
+	.upload("./public/" + file, function(err, result) {
+        console.log(result);
+
+
+		var user = db.get('users')
+		.find({ id: updateId})
+		.assign({ name: req.body.name, 
+					email: req.body.email,
+					avatarURL: file
+					})
+		.write();
+
+		res.redirect('/users/index')
+	});
+	
+}
+		
+	
+
+
+
--- router/users.route.js
+++ router/users.route.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var multer  = require('multer')
 var validate = require('../validate/user.validate');
 var controller = require('../controllers/users.controllers');
 
+var upload = multer({ dest: './public/uploads/' })
 
 var router = express.Router();
 
@@ -11,9 +13,12 @@ router.get('/:id/delete',controller.delete);
 
 router.get('/:id/update',controller.update);
 
-router.post('/:id/update', controller.postUpdate);
+router.post('/:id/update', upload.single('avatar'), controller.postUpdate);
 
-router.post('/index', validate.postIndex, controller.postIndex);
+router.post('/index', upload.single('avatar'), 
+	validate.postIndex,
+	 controller.postIndex
+	 );
 
 
 module.exports = router;
