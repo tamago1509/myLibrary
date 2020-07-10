1e5424c925c9acdcc322e5fcac653df5ec5be2d2
tamago1509 <54825087+tamago1509@users.noreply.github.com>
2020-07-10 09:40:47 +0700

    change db

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ mongoose
     useUnifiedTopology: true
 })
 
-const db = mongoose.connection;
+db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("connected to mongo Atlas")
